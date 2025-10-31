93f758c62a3c1c2bf5617551f54bac280419dce9
RADCAT235 <rafal.mad1234@gmail.com>
2025-10-31 13:12:50 +0100

    zadanie 2.1, nowe folderowania

---
+++ zadania/programy_Temat2/zad_2.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "podaj wynik:\n";
+	int punkty; 
+    cin >> punkty;
+    
+    if (punkty == 100) cout << "ocena celujoca";
+    else if (punkty >= 85 && punkty <= 99 )cout << "ocena bardzo dobra";
+    else if (punkty >= 70 && punkty <= 84 )cout << "ocena dobra";
+    else if (punkty >= 50 && punkty <= 69 )cout << "ocena dostateczna";
+    else if (punkty >= 0 && punkty <= 49 )cout << "ocena niedosateczna";
+    else if (punkty < 0 || punkty > 100 )cout << "blod, zakres od 0 do 100, wpisales: "<< punkty;
+    
+    return 0;
+}
