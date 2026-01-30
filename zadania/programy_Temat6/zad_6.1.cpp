f05564fc90df9cdd30af3f69b36f502235fc99ff
RADCAT235 <rafal.mad1234@gmail.com>
2026-01-30 10:49:00 +0100

    Update zad_6.1.cpp

--- zadania/programy_Temat6/zad_6.1.cpp
+++ zadania/programy_Temat6/zad_6.1.cpp
@@ -3,20 +3,15 @@ using namespace std;
 int main(){
 	
 	int tablica[5];
+ float suma = 0;
 	
 	cout << "Podaj 5 liczb calkowitych:\n";
 	for (int i = 0; i < 5; i++) {
 		cout << "Podaj liczbe nr " << i + 1 << ": ";
 		cin >> tablica[i];
+  suma += tablica[i];
 	}
-	
-	float suma = 0;
 
-	for (int i = 0; i < 5; i++){
-		suma += tablica[i];
-	}
-	
 	cout << "\nsrednia wynosi: " << suma/5 << endl;
-
 
 }
