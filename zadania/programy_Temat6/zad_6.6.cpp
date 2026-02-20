603b95ffe5324d31b426fd7210db611a247d8b24
RADCAT235 <rafal.mad1234@gmail.com>
2026-02-20 10:50:46 +0100

    dodanie zadan 6.5-6.7, poprawa 6.5

--- zadania/programy_Temat6/zad_6.6.cpp
+++ zadania/programy_Temat6/zad_6.6.cpp
@@ -11,16 +11,16 @@ int main(){
 	cin >> y;
 	
 	int liczby[x][y];
-	int sredna =0;
+	int suma =0;
 	
-	cout <<"\npodajliczby calkowite dla macierzy "<<x<<":"<<y<<endl<<endl;
+	cout <<"\npodaj liczby calkowite dla macierzy "<<x<<":"<<y<<endl<<endl;
 	for (int i = 0; i < x; i++) {
 		for(int j=0;j <y; j++){
 			cout << "Podaj liczbe o koordynatach: "<<i+1<<":"<<j+1<<" : ";
 			cin >> liczby[i][j];
-			sredna += liczby[i][j];
+			suma += liczby[i][j];
 		}
 	}
-	cout <<"sredna podanych liczb: "<< sredna/9;
+	cout <<"suma podanych liczb: "<< suma;
 
 }
